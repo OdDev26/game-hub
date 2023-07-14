776e5fefe713c02b8d15c516c4389d77026de088
Michael Egbe-Iyon <Michael.Egbe-Iyon@vfdgroupltd.com>
2023-07-14 15:03:46 +0100

    Built a nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <Button colorScheme="blue"></Button>;
+  return (
+    <>
+      <Grid
+        templateAreas={{
+          base: `"nav  main"`, // defines columns (nav & main) to show on small screens
+          lg: `"nav nav" "aside main"`, // specifies that we want to see all columns (nav, aside and main) on large screens only (With size above 1024px)
+        }}
+      >
+        <GridItem area="nav">
+          <NavBar />
+        </GridItem>
+        <Show above="lg">
+          {" "}
+          {/**This ensures aside area is only shown on large screens */}
+          <GridItem bg="blue" area="aside">
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem bg="orange" area="main">
+          Main
+        </GridItem>
+      </Grid>
+    </>
+  );
 }
 
 export default App;

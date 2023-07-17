e4b0e9aa1a3cb753f4641a70ed85f834ab09fe17
Michael Egbe-Iyon <Michael.Egbe-Iyon@vfdgroupltd.com>
2023-07-17 22:37:38 +0100

    added styles peculiar to GameCard and GameCard skeleton to one component

---
+++ src/components/GameCardContainer.tsx
@@ -0,0 +1,17 @@
+import { Box } from "@chakra-ui/react";
+import React, { ReactNode } from "react";
+interface Props {
+  children: ReactNode;
+}
+// We create this component to hold the common styles of both the GameCard and GameCardSkeleton Components
+const GameCardContainer = ({ children }: Props) => {
+  return (
+    <>
+      <Box borderRadius={10} overflow="hidden" width="300px">
+        {children}
+      </Box>
+    </>
+  );
+};
+
+export default GameCardContainer;
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,6 +3,7 @@ import useGames from "../hooks/useGames";
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
   const { gameData, error, isLoading } = useGames();
@@ -17,9 +18,17 @@ const GameGrid = () => {
       >
         {" "}
         {/** With this on small screens(e.g a mobile device) we would have 1 column while on medium screens e.g tablet we would have 2 columns and so on */}
-        {isLoading && skeletons.map((item) => <GameCardSkeleton key={item} />)}
+        {isLoading &&
+          skeletons.map((item) => (
+            <GameCardContainer>
+              <GameCardSkeleton key={item} />
+            </GameCardContainer>
+          ))}
         {gameData.map((game) => (
-          <GameCard key={game.id} game={game} />
+          <GameCardContainer>
+            {" "}
+            <GameCard key={game.id} game={game} />
+          </GameCardContainer>
         ))}
       </SimpleGrid>
     </>

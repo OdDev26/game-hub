e004c0c649b23e8d5cc636310054c08c13988b92
Michael Egbe-Iyon <Michael.Egbe-Iyon@vfdgroupltd.com>
2023-07-19 21:06:08 +0100

    Handling null image urls

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
-import logo from "../assets/logo.jpg";
+import logo from "../assets/logo.webp"; // we do this because the image is in a static assets folder(that would be deployed with the app)
 import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {

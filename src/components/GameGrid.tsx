7029aa7d3737cbe8854479153074f63f38719b93
Michael Egbe-Iyon <Michael.Egbe-Iyon@vfdgroupltd.com>
2023-07-19 09:55:07 +0100

    Added a spinner for genre list

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,12 +12,16 @@ const GameGrid = () => {
     <>
       {error && <Text color="red">{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        columns={{
+          sm: 1,
+          md: 2,
+          lg: 3,
+          xl: 5,
+        }} /** With this on small screens(e.g a mobile device) we would have 1 column while on medium screens e.g tablet we would have 2 columns and so on */
         padding="10px"
         spacing={3} /**This handles the space between the GameCards**/
       >
         {" "}
-        {/** With this on small screens(e.g a mobile device) we would have 1 column while on medium screens e.g tablet we would have 2 columns and so on */}
         {isLoading &&
           skeletons.map((item) => (
             <GameCardContainer>
